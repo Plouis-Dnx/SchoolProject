28c95dc8891031a81d3d178304c7253f00cd9a25
WildRock <plouis.deneux@gmail.com>
2024-11-16 11:14:05 +0100

    AVOIR.sql modifié

---
+++ Base de Données/AVOIR.sql
@@ -0,0 +1,199 @@
+INSERT INTO AVOIR VALUES
+--(idFilm, idActeur, idRéalisateur, thèmes)
+
+--Blade Runner
+(1,1,1,'Science Fiction'),
+(1,2,1,'Science Fiction'),
+(1,3,1,'Science Fiction'),
+(1,4,1,'Science Fiction'),
+(1,5,1,'Science Fiction'),
+
+--Matrix
+(2,6,2,'Science Fiction'),
+(2,7,2,'Science Fiction'),
+(2,8,2,'Science Fiction'),
+(2,9,2,'Science Fiction'),
+(2,10,2,'Science Fiction'),
+(2,6,3,'Science Fiction'),
+(2,7,3,'Science Fiction'),
+(2,8,3,'Science Fiction'),
+(2,9,3,'Science Fiction'),
+(2,10,3,'Science Fiction'),
+
+--Alien
+(3,11,4,'Science Fiction'),
+(3,12,4,'Science Fiction'),
+(3,13,4,'Science Fiction'),
+(3,14,4,'Science Fiction'),
+(3,15,4,'Science Fiction'),
+
+--Inception
+(4,16,5,'Science Fiction'),
+(4,17,5,'Science Fiction'),
+(4,18,5,'Science Fiction'),
+(4,19,5,'Science Fiction'),
+(4,20,5,'Science Fiction'),
+(4,21,5,'Science Fiction'),
+
+--Interstellar
+(5,22,6,'Science Fiction'),
+(5,23,6,'Science Fiction'),
+(5,24,6,'Science Fiction'),
+
+--Ready Player One
+(6,25,7,'Fantastique'),
+(6,26,7,'Fantastique'),
+(6,27,7,'Fantastique'),
+(6,28,7,'Fantastique'),
+(6,29,7,'Fantastique'),
+
+--Avatar
+(7,30,8,'Fantastique'),
+(7,31,8,'Fantastique'),
+(7,32,8,'Fantastique'),
+(7,33,8,'Fantastique'),
+(7,34,8,'Fantastique'),
+
+--Pirates des Caraïbes : La Malédiction du Black Pearl
+(8,35,9,'Fantastique'),
+(8,36,9,'Fantastique'),
+(8,37,9,'Fantastique'),
+(8,38,9,'Fantastique'),
+(8,39,9,'Fantastique'),
+
+--Dune
+(9,40,10,'Fantastique'),
+(9,41,10,'Fantastique'),
+(9,42,10,'Fantastique'),
+(9,43,10,'Fantastique'),
+(9,44,10,'Fantastique'),
+
+--Spaceman
+(10,45,11,'Aventure'),
+(10,46,11,'Aventure'),
+(10,47,11,'Aventure'),
+(10,48,11,'Aventure'),
+
+--La Planète des singes : Les Origines
+(11,49,12,'Aventure'),
+(11,50,12,'Aventure'),
+(11,51,12,'Aventure'),
+(11,52,12,'Aventure'),
+(11,53,12,'Aventure'),
+
+--Everest
+(12,54,13,'Aventure'),
+(12,55,13,'Aventure'),
+(12,56,13,'Aventure'),
+(12,57,13,'Aventure'),
+(12,58,13,'Aventure'),
+
+--Les Trois Mousquetaires : D Artagnan
+(13,59,14,'Aventure'),
+(13,60,14,'Aventure'),
+(13,61,14,'Aventure'),
+(13,62,14,'Aventure'),
+
+--Percy Jackson : Le Voleur de Foudre
+(14,63,15,'Aventure'),
+(14,64,15,'Aventure'),
+(14,65,15,'Aventure'),
+(14,66,15,'Aventure'),
+(14,67,15,'Aventure'),
+
+--James Bond : Skyfall
+(15,69,16,'Action'),
+(15,70,16,'Action'),
+(15,71,16,'Action'),
+(15,72,16,'Action'),
+(15,73,16,'Action'),
+
+--John Wick
+(16,74,17,'Action'),
+(16,75,17,'Action'),
+(16,76,17,'Action'),
+(16,77,17,'Action'),
+(16,78,17,'Action'),
+(16,79,17,'Action'),
+
+--Fast & Furious 7
+(17,80,18,'Action'),
+(17,81,18,'Action'),
+(17,82,18,'Action'),
+(17,83,18,'Action'),
+(17,80,19,'Action'),
+(17,81,19,'Action'),
+(17,82,19,'Action'),
+(17,83,19,'Action'),
+
+--The Kingsman : Première Mission
+(18,84,20,'Action'),
+(18,85,20,'Action'),
+(18,86,20,'Action'),
+(18,87,20,'Action'),
+(18,88,20,'Action'),
+
+--Top Gun : Maverick
+(19,89,21,'Action'),
+(19,90,21,'Action'),
+(19,91,21,'Action'),
+(19,92,21,'Action'),
+
+--The Revenant
+(20,93,22,'Thriller'),
+(20,94,22,'Thriller'),
+(20,95,22,'Thriller'),
+(20,96,22,'Thriller'),
+
+--Le Monde après Nous
+(21,97,23,'Thriller'),
+(21,98,23,'Thriller'),
+(21,99,23,'Thriller'),
+(21,100,23,'Thriller'),
+
+--Le Chant du Loup
+(22,101,24,'Thriller'),
+(22,102,24,'Thriller'),
+(22,103,24,'Thriller'),
+
+--Da Vinci Code
+(23,104,23,'Thriller'),
+(23,105,23,'Thriller'),
+(23,106,23,'Thriller'),
+(23,107,23,'Thriller'),
+
+--American Nightmare
+(24,108,26,'Thriller'),
+(24,109,26,'Thriller'),
+(24,110,26,'Thriller'),
+(24,111,26,'Thriller'),
+
+--Smile
+(25,112,27,'Horreur'),
+(25,113,27,'Horreur'),
+(25,114,27,'Horreur'),
+(25,115,27,'Horreur'),
+
+--Saw
+(26,116,28,'Horreur'),
+(26,117,28,'Horreur'),
+(26,118,28,'Horreur'),
+
+--Scream
+(27,119,29,'Horreur'),
+(27,119,29,'Horreur'),
+(27,119,29,'Horreur'),
+(27,119,29,'Horreur'),
+
+--La Nonne
+(28,123,30,'Horreur'),
+(28,123,30,'Horreur'),
+(28,123,30,'Horreur'),
+(28,123,30,'Horreur'),
+
+
+
+
+
+
+
