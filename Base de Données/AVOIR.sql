54a4aefa93cba07334acc14e4145976b3a0a236e
Natacha62 <150607094+Natacha62@users.noreply.github.com>
2024-11-16 12:28:46 +0100

    Update AVOIR.sql

--- Base de Données/AVOIR.sql
+++ Base de Données/AVOIR.sql
@@ -73,16 +73,16 @@ INSERT INTO AVOIR VALUES
 (10,46,11,'Aventure'),
 (10,47,11,'Aventure'),
 (10,48,11,'Aventure'),
+(10,49,11,'Aventure'),
 
 --La Planète des singes : Les Origines
-(11,49,12,'Aventure'),
 (11,50,12,'Aventure'),
 (11,51,12,'Aventure'),
 (11,52,12,'Aventure'),
 (11,53,12,'Aventure'),
+(11,54,12,'Aventure'),
 
 --Everest
-(12,54,13,'Aventure'),
 (12,55,13,'Aventure'),
 (12,56,13,'Aventure'),
 (12,57,13,'Aventure'),
@@ -93,13 +93,14 @@ INSERT INTO AVOIR VALUES
 (13,60,14,'Aventure'),
 (13,61,14,'Aventure'),
 (13,62,14,'Aventure'),
+(13,63,14,'Aventure'),
 
 --Percy Jackson : Le Voleur de Foudre
-(14,63,15,'Aventure'),
 (14,64,15,'Aventure'),
 (14,65,15,'Aventure'),
 (14,66,15,'Aventure'),
 (14,67,15,'Aventure'),
+(14,68,15,'Aventure'),
 
 --James Bond : Skyfall
 (15,69,16,'Action'),
@@ -107,93 +108,197 @@ INSERT INTO AVOIR VALUES
 (15,71,16,'Action'),
 (15,72,16,'Action'),
 (15,73,16,'Action'),
+(15,74,16,'Action'),
 
 --John Wick
-(16,74,17,'Action'),
 (16,75,17,'Action'),
 (16,76,17,'Action'),
 (16,77,17,'Action'),
 (16,78,17,'Action'),
-(16,79,17,'Action'),
+(16,79,18,'Action'),
+(16,80,18,'Action'),
+(16,81,18,'Action'),
+(16,82,18,'Action'),
 
 --Fast & Furious 7
-(17,80,18,'Action'),
-(17,81,18,'Action'),
-(17,82,18,'Action'),
-(17,83,18,'Action'),
-(17,80,19,'Action'),
-(17,81,19,'Action'),
-(17,82,19,'Action'),
 (17,83,19,'Action'),
+(17,84,19,'Action'),
+(17,85,19,'Action'),
+(17,86,19,'Action'),
+(17,87,19,'Action'),
 
 --The Kingsman : Première Mission
-(18,84,20,'Action'),
-(18,85,20,'Action'),
-(18,86,20,'Action'),
-(18,87,20,'Action'),
 (18,88,20,'Action'),
+(18,89,20,'Action'),
+(18,90,20,'Action'),
+(18,91,20,'Action'),
 
 --Top Gun : Maverick
-(19,89,21,'Action'),
-(19,90,21,'Action'),
-(19,91,21,'Action'),
 (19,92,21,'Action'),
+(19,93,21,'Action'),
+(19,94,21,'Action'),
+(19,95,21,'Action'),
 
 --The Revenant
-(20,93,22,'Thriller'),
-(20,94,22,'Thriller'),
-(20,95,22,'Thriller'),
 (20,96,22,'Thriller'),
+(20,97,22,'Thriller'),
+(20,98,22,'Thriller'),
+(20,99,22,'Thriller'),
 
 --Le Monde après Nous
-(21,97,23,'Thriller'),
-(21,98,23,'Thriller'),
-(21,99,23,'Thriller'),
 (21,100,23,'Thriller'),
+(21,101,23,'Thriller'),
+(21,102,23,'Thriller'),
 
 --Le Chant du Loup
-(22,101,24,'Thriller'),
-(22,102,24,'Thriller'),
 (22,103,24,'Thriller'),
+(22,104,24,'Thriller'),
+(22,105,24,'Thriller'),
+(22,106,24,'Thriller'),
 
 --Da Vinci Code
-(23,104,23,'Thriller'),
-(23,105,23,'Thriller'),
-(23,106,23,'Thriller'),
 (23,107,23,'Thriller'),
+(23,108,23,'Thriller'),
+(23,109,23,'Thriller'),
+(23,110,23,'Thriller'),
 
 --American Nightmare
-(24,108,26,'Thriller'),
-(24,109,26,'Thriller'),
-(24,110,26,'Thriller'),
 (24,111,26,'Thriller'),
+(24,112,26,'Thriller'),
+(24,113,26,'Thriller'),
+(24,114,26,'Thriller'),
 
 --Smile
-(25,112,27,'Horreur'),
-(25,113,27,'Horreur'),
-(25,114,27,'Horreur'),
 (25,115,27,'Horreur'),
+(25,116,27,'Horreur'),
+(25,117,27,'Horreur'),
 
 --Saw
-(26,116,28,'Horreur'),
-(26,117,28,'Horreur'),
 (26,118,28,'Horreur'),
+(26,119,28,'Horreur'),
+(26,120,28,'Horreur'),
+(26,121,28,'Horreur'),
 
 --Scream
-(27,119,29,'Horreur'),
-(27,119,29,'Horreur'),
-(27,119,29,'Horreur'),
-(27,119,29,'Horreur'),
+(27,122,29,'Horreur'),
+(27,123,29,'Horreur'),
+(27,124,29,'Horreur'),
+(27,125,29,'Horreur'),
 
 --La Nonne
-(28,123,30,'Horreur'),
-(28,123,30,'Horreur'),
-(28,123,30,'Horreur'),
-(28,123,30,'Horreur'),
+(28,126,30,'Horreur'),
+(28,127,30,'Horreur'),
+(28,128,30,'Horreur'),
 
+--Conjuring
+(29,129,31,'Horreur'),
+(29,130,31,'Horreur'),
+(29,131,31,'Horreur'),
+(29,132,31,'Horreur'),
 
+--Camping
+(30,133,32,'Comédie'),
+(30,134,32,'Comédie'),
+(30,135,32,'Comédie'),
+(30,136,32,'Comédie'),
 
+--Astérix et Obélix
+(31,137,33,'Comédie'),
+(31,138,33,'Comédie'),
+(31,139,33,'Comédie'),
 
+--Les Tuches
+(32,140,34,'Comédie'),
+(32,141,34,'Comédie'),
+(32,142,34,'Comédie'),
+(32,143,34,'Comédie'),
 
+--Les Profs
+(33,144,35,'Comédie'),
+(33,145,35,'Comédie'),
+(33,146,35,'Comédie'),
 
+--Rien à déclarer
+(34,147,36,'Comédie'),
+(34,148,36,'Comédie'),
+(34,149,36,'Comédie'),
+(34,150,36,'Comédie'),
 
+--Maigret
+(35,151,37,'Policier'),
+(35,152,37,'Policier'),
+(35,153,37,'Policier'),
+
+--Bac Nord
+(36,154,38,'Policier'),
+(36,155,38,'Policier'),
+(36,156,38,'Policier'),
+
+--À Couteaux Tirés
+(37,157,39,'Policier'),
+(37,158,39,'Policier'),
+(37,159,39,'Policier'),
+(37,160,39,'Policier'),
+
+--Copycat
+(38,161,40,'Policier'),
+(38,162,40,'Policier'),
+(38,163,40,'Policier'),
+
+--Criminal Squad
+(39,164,41,'Policier'),
+(39,165,41,'Policier'),
+(39,166,41,'Policier'),
+
+--Le Bon, la Brute et le Truand
+(40,167,42,'Western'),
+(40,168,42,'Western'),
+(40,169,42,'Western'),
+
+--Django Unchained
+(41,170,43,'Western'),
+(41,171,43,'Western'),
+(41,172,43,'Western'),
+(41,173,43,'Western'),
+
+--Les Huit Salopards
+(42,174,44,'Western'),
+(42,175,44,'Western'),
+(42,176,44,'Western'),
+(42,177,44,'Western'),
+
+--La Ballade de Buster Scruggs
+(43,178,45,'Western'),
+(43,179,45,'Western'),
+(43,180,45,'Western'),
+(43,181,45,'Western'),
+(43,182,46,'Western'),
+(43,183,46,'Western'),
+(43,184,46,'Western'),
+(43,185,46,'Western'),
+
+--Gran Torino
+(44,186,47,'Western'),
+(44,187,47,'Western'),
+(44,188,47,'Western'),
+
+--Océans
+(45,189,48,'Documentaires'),
+(45,190,48,'Documentaires'),
+(45,191,49,'Documentaires'),
+(45,192,49,'Documentaires'),
+
+--Félins
+(46,193,50,'Documentaires'),
+(46,194,50,'Documentaires'),
+(46,195,51,'Documentaires'),
+(46,196,51,'Documentaires'),
+
+--La Marche de l'Empereur
+(47,197,52,'Documentaires'),
+(47,198,52,'Documentaires'),
+
+--La Chute
+(48,199,53,'Documentaires'),
+(48,200,53,'Documentaires'),
+(48,201,53,'Documentaires'),
